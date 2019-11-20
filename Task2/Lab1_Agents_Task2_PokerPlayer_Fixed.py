971abe7da55ebc377e80e1aaf74a9c002fee5008
Kalle <kalle@djervbrant.net>
2019-11-20 17:21:41 +0100

    Rebase

--- Task2/Lab1_Agents_Task2_PokerPlayer_Fixed.py
+++
@@ -1,6 +0,0 @@
-from Lab1_Agents_Task2_PokerPlayer import PokerPlayer
-
-class FixedPlayer(PokerPlayer):
-
-    def bet(self):
-        return 25
--- Task2/Lab1_Agents_Task2_PokerPlayer_random.py
+++
@@ -1,9 +0,0 @@
-import random
-from Lab1_Agents_Task2_PokerPlayer import PokerPlayer
-
-class RandomPlayer(PokerPlayer):
-
-    def bet(self):
-        return random.randint(0,50)
-        
-
