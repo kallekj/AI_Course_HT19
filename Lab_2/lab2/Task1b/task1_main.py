bf736d1bd78aeba41e896557ef02c3a7702143ff
Kalle <kalle@djervbrant.net>
2019-12-08 19:24:39 +0100

    Work in progress on the report

--- Lab_2/lab2/Task1b/task1_main.py
+++ Lab_2/lab2/Task1b/task1_main.py
@@ -18,7 +18,8 @@ AStar_Man = []
 AStar_Euc = []
 #AStar_Cust_E = []
 AStar_Cust_M = []
-
+Greedy_M_Data = []
+Greedy_E_Data = []
 
 for i in range(100):
     print(i)
@@ -26,11 +27,16 @@ for i in range(100):
     AStar_Man_Agent = AstarM()
     AStar_Euc_Agent = AstarE()
     AStar_Cust_M_Agent = AstarC()
+    Greedy_agent_m = GreedySearchM()
+    Greedy_agent_e = GreedySearchE()
+
 
     map_object, info = pp.generateMap2d_obstacle([100, 100])
     map_object2 = np.copy(map_object)
     map_object3 = np.copy(map_object)
     map_object4 = np.copy(map_object)
+    map_object5 = np.copy(map_object)
+    map_object6 = np.copy(map_object)
 
     start = Node([np.where(map_object == -2)[0][0], np.where(map_object == -2)[1][0]], None, 0, 0)
     goal = Node([np.where(map_object == -3)[0][0], np.where(map_object == -3)[1][0]], None, 0, 0)
@@ -39,11 +45,15 @@ for i in range(100):
     AStar_Euc_Agent.search(map_object2, start, goal)
     AStar_Cust_M_Agent.search(map_object3, start, goal, info)
     BFSAgent.search(map_object4, start, goal)
+    Greedy_agent_m.search(map_object5, start, goal)
+    Greedy_agent_e.search(map_object6, start, goal)
 
     BFS_Data.append({"nodes":BFSAgent.searchedNodes, "pathLength":len(BFSAgent.path[0]) + len(BFSAgent.path[1])})
     AStar_Man.append({"nodes":AStar_Man_Agent.searchedNodes, "pathLength":len(AStar_Man_Agent.path[0]) + len(AStar_Man_Agent.path[1])})
     AStar_Euc.append({"nodes":AStar_Euc_Agent.searchedNodes, "pathLength":len(AStar_Euc_Agent.path[0]) + len(AStar_Euc_Agent.path[1])})
     AStar_Cust_M.append({"nodes":AStar_Cust_M_Agent.searchedNodes, "pathLength":len(AStar_Cust_M_Agent.path[0]) + len(AStar_Cust_M_Agent.path[1])})
+    Greedy_M_Data.append({"nodes":Greedy_agent_m.searchedNodes, "pathLength":len(Greedy_agent_m.path[0]) + len(Greedy_agent_m.path[1])})
+    Greedy_E_Data.append({"nodes":Greedy_agent_e.searchedNodes, "pathLength":len(Greedy_agent_e.path[0]) + len(Greedy_agent_e.path[1])})
     
 
 BFS_DF = pd.DataFrame(BFS_Data)
@@ -54,6 +64,12 @@ AStarE_DF = pd.DataFrame(AStar_Euc)
 AStarE_Mean = AStarE_DF.mean(axis=0)
 AStarC_DF = pd.DataFrame(AStar_Cust_M)
 AStarC_Mean = AStarC_DF.mean(axis=0)
+GreedyM_DF = pd.DataFrame(Greedy_M_Data)
+GreedyM_Mean = GreedyM_DF.mean(axis=0) 
+GreedyE_DF = pd.DataFrame(Greedy_E_Data)
+GreedyE_Mean = GreedyE_DF.mean(axis=0) 
+
+
 
 print("\n       BFS Mean")
 print(BFS_Mean)
@@ -63,6 +79,10 @@ print("\n       A* Euclidean Mean")
 print(AStarE_Mean)
 print("\n       A* Custom Mean")
 print(AStarC_Mean)
+print("\n       Greedy Manhattan Mean")
+print(GreedyM_Mean)
+print("\n       Greedy Euclidean Mean")
+print(GreedyE_Mean)
 
 
 
@@ -74,6 +94,10 @@ print("")
 print(AStarE_Mean.to_latex())
 print("")
 print(AStarC_Mean.to_latex())
+print("")
+print(GreedyM_Mean.to_latex())
+print("")
+print(GreedyE_Mean.to_latex())
 
 
 # map_object, info = pp.generateMap2d_obstacle([100, 100])
